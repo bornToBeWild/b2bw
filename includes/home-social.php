59092cd74fe7e97fffd32e7e3ca6b35086d4364f
Valériane Aron <valeriane.aron@gmail.com>
2017-10-13 22:45:29 +0200

    adding of section social networks buttons, first element of index to include

---
+++ includes/home-social.php
@@ -0,0 +1,28 @@
+<section class="social-buttons-section">
+        <div id="social-container">
+          <div class="social">
+            <a href="https://www.facebook.com/" target="_blank">
+              <img src="assets/pictures/socialIcon/pictofb.jpg" alt="Facebook pictogram"/>
+            </a> 
+          </div>
+
+              <div class="social">
+                <a href="https://twitter.com/?lang=fr" target="_blank">
+                  <img src="assets/pictures/socialIcon/pictotwit.jpg" alt="Twitter pictogram"/>
+                </a>  
+              </div>
+
+              <div class="social">
+                <a href="https://www.instagram.com/?hl=fr" target="_blank">
+                  <img src="assets/pictures/socialIcon/pictoinsta.jpg" alt="Instagram pictogram"/>
+                </a>    
+              </div>
+
+              <div class="social">
+                <a href="https://www.spotify.com/fr/" target="_blank">
+                  <img src="assets/pictures/socialIcon/pictospotify.jpg" alt="Spotify pictogram"/>
+                </a> 
+              </div>
+
+            </div>
+      </section>
