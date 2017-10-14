5cc7011a9a3998853f0a8b260e4cb5ad1ace920b
Pandora <pandoraangora@gmail.com>
2017-10-14 20:31:32 +0200

    modify contact and css

--- contact.php
+++ contact.php
@@ -1,26 +1,68 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/home-social.php'); ?>
-
+<?php include('includes/home-section-header.php'); ?>
 
 <body>
-  
-<img src="assets/pictures/background.jpg">
-  <h1>Contact us</h1>
-    <p>Please contact us for any information.</p><form method="post" action="process.php">
-      <p>
-        <label>Name</label><input type="text" name="name" id="name" required placeholder="your name"> 
-      </p> 
-      <p>
-        <label>Email</label><input type="email" name="email" id="email" placeholder="your email address" required> 
-      </p> 
-      <p>
-        <label for="message">Message</label> <br />
-        <textarea name="message" id="message" rows="10" cols="50"></textarea>
-      </p>
-      <input type="submit" value="Submit">
-      <p>For press relation please send us an email to <a href="press@b2bw.com"></a> press@b2bw.com</p>
 
+  <section class="contact">
+    <h1>Contact us</h1>
+    <p>Please contact us for any information.</p>
+  </section>
+    <form class="form-horizontal" method="post" action="contact_success.php">
+      <fieldset>
 
-</form>
+        <!-- Name-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="textinput">name</label>
+          <div class="col-md-4">
+            <input id="textinput" name="textinput" placeholder="name" class="form-control input-md" required="" type="text">
+
+          </div>
+        </div>
+
+        <!-- Email-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="textinput">email</label>
+          <div class="col-md-4">
+            <input id="textinput" name="textinput" placeholder="email" class="form-control input-md" required="" type="text">
+
+          </div>
+        </div>
+
+        <!-- Multiple Radios -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="radios">Are you a professionnal?</label>
+          <div class="col-md-4">
+            <div class="radio">
+              <label for="radios-0">
+                <input name="radios" id="radios-0" value="1" checked="checked" type="radio"> yes
+              </label>
+            </div>
+            <div class="radio">
+              <label for="radios-1">
+                <input name="radios" id="radios-1" value="2" type="radio"> no
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <!-- Message -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="message">Message</label>
+          <div class="col-md-4">
+            <textarea class="form-control" id="message" name="message"></textarea>
+          </div>
+        </div>
+
+        <!-- Button -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="singlebutton"></label>
+          <div class="col-md-4">
+            <button id="singlebutton" name="singlebutton" class="btn btn-default">Submit</button>
+          </div>
+        </div>
+
+      </fieldset>
+    </form>
 </body>
 <?php include('includes/footer.php'); ?>
