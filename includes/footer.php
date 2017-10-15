b3f23e10cc06629c263e43ca7593a9299f822a39
Anysia <abeauzel@orange.fr>
2017-10-15 19:22:32 +0200

    add footer.php

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<footer>
+	
+	<div class="flex_container">
+
+			<ul class="box">
+				<li>Home</li>
+				<li>About Us</li>
+				<li>News</li>
+				<li>Medias</li>
+			</ul>
+		
+			<ul class="box">
+				<li>Contact Us</li>
+				<li>FAQ</li>
+				<li>Legal Mentions</li>
+			</ul>
+
+		<div class="with_love">
+			<p>Made with <span class="heart">❤</span> by B2BW</p>
+			<p>2017</p>
+		</div>
+
+			<ul class="box">
+				<li>Facebook</li>
+				<li>Instagram</li>
+			</ul>
+
+			<ul class="box">
+				<li>Twitter</li>
+				<li>Spotify</li>
+			</ul>
+
+	</div>
+
+</footer>
