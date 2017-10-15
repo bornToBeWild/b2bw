1c516fe452ffacf28e75d7129e3db3e6a8de8f00
Pandora <pandoraangora@gmail.com>
2017-10-15 03:41:39 +0200

    modify contact

--- contact.php
+++ contact.php
@@ -69,16 +69,6 @@
     </form>
   </div>
 
-<!-- Newsletter -->
-<aside>
-  <div class="newsletter col-md-offset-8"> 
-      <h2>JOIN THE NEWSLETTER</h2>
-        <div class="input-container">
-          <input type="text" placeholder="your email" class="email text-input">
-          <button id="singlebutton" name="singlebutton" class="btn btn-default">SUBSCRIBE</button>
-      </div>
-  </div>
-</aside>
 </body>
 <?php include('includes/footer.php'); ?>
 
