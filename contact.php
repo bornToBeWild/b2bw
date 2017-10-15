729096c2ce030a222fe33e7e6883b134cce0212b
Pandora <pandoraangora@gmail.com>
2017-10-15 03:33:29 +0200

    add newsletter

--- contact.php
+++ contact.php
@@ -68,6 +68,17 @@
       </fieldset>
     </form>
   </div>
+
+<!-- Newsletter -->
+<aside>
+  <div class="newsletter col-md-offset-8"> 
+      <h2>JOIN THE NEWSLETTER</h2>
+        <div class="input-container">
+          <input type="text" placeholder="your email" class="email text-input">
+          <button id="singlebutton" name="singlebutton" class="btn btn-default">SUBSCRIBE</button>
+      </div>
+  </div>
+</aside>
 </body>
 <?php include('includes/footer.php'); ?>
 
---
+++ includes/newsletter.php
@@ -0,0 +1,11 @@
+<!-- Newsletter -->
+<aside>
+  <div class="newsletter col-md-offset-8"> 
+      <h2>JOIN THE NEWSLETTER</h2>
+        <div class="input-container">
+          <input type="text" placeholder="your email" class="email text-input">
+          <button id="singlebutton" name="singlebutton" class="btn btn-default">SUBSCRIBE</button>
+      </div>
+  </div>
+</aside>
+
