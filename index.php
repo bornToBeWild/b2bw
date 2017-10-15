57b4573abd233628afc30eb6435485196f6d1440
Anysia <abeauzel@orange.fr>
2017-10-15 19:20:26 +0200

    needed to change index.html to index.php to use include

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<link rel="stylesheet" href="assets/css/stylefooter.css" />
+
+		<!--Bootsrap library-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	</head>
+
+<body>
+		
+	<?php include 'includes/footer.php'?>
+
+</body>
+</html>
