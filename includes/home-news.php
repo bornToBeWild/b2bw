07c1da5cdf058534aa06d9e0245f42c01a2517ca
Anysia <abeauzel@orange.fr>
2017-10-17 14:22:03 +0200

    add homepage-news files: include, css and pictures

---
+++ includes/home-news.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>BornToBeWild</title>
+
+		<!-- Immport Bootstrap library-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="home-news.css" />
+
+		<!--Favicon-->
+		<link rel="shortcut icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
+		<link rel="icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
+
+		<!-- Importation des typos Google Fonts, en css : 
+		font-family: 'Quicksand', sans-serif; = pour le corps de texte & Titres TBC
+		font-family: 'Josefin Sans', sans-serif; = divers
+		font-family: 'Yanone Kaffeesatz', sans-serif; = divers	
+		font-family: 'Abel', sans-serif; = divers
+		font-family: 'Amatic SC', cursive; = pour la nav bar-->
+		<link href="https://fonts.googleapis.com/css?family=Abel|Amatic+SC|Josefin+Sans|Quicksand|Yanone+Kaffeesatz" rel="stylesheet">
+	</head>
+<body>
+
+
+	<section>
+
+		<h2>News</h2>
+
+		<div class="hpnews_container">
+
+			<figure>
+				<img src="images/Wild1.jpg" alt="Steppenwolf">
+				<figcaption>
+					<p>Born to be Wild Announces Their first Tour: Europe!</p>
+					<p>A full production and an awesome line up awaits...
+					<a href="#" class="more">[+]</a>
+					</p>
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img src="images/Wild2.jpg" alt="Lou Reed">
+				<figcaption>
+					<p>New pictures online!</p>
+					<p>Our photo gallery editor is finally finished which gives us...
+					<a href="#" class="more">[+]</a>
+					</p>
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img src="images/Wild3.jpg" alt="LP">
+				<figcaption>
+					<p>Born to be Wild Merchandise</p>
+					<p>From now on you can order your Born to be Wild merch...
+					<a href="#" class="more">[+]</a>
+					</p>
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img src="images/Wild4.jpg" alt="Wild Records">
+				<figcaption>
+					<p>Official Born to be Wild web site launches!</p>
+					<p>A brand new album calls for a brand new site...
+					<a href="#" class="more">[+]</a>
+					</p>
+				</figcaption>
+			</figure>
+		</div>
+	
+
+
+
+
+
+
+
+
+
+
+
+	</section>
