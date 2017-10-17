aafddfc81e3ba725f86a674b91772f47792e7b2e
Anysia <abeauzel@orange.fr>
2017-10-17 14:52:50 +0200

    removed the Doctype

--- includes/home-news.php
+++ includes/home-news.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>BornToBeWild</title>
-
-		<!-- Immport Bootstrap library-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<link rel="stylesheet" href="home-news.css" />
-
-		<!--Favicon-->
-		<link rel="shortcut icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
-		<link rel="icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
-
-		<!-- Importation des typos Google Fonts, en css : 
-		font-family: 'Quicksand', sans-serif; = pour le corps de texte & Titres TBC
-		font-family: 'Josefin Sans', sans-serif; = divers
-		font-family: 'Yanone Kaffeesatz', sans-serif; = divers	
-		font-family: 'Abel', sans-serif; = divers
-		font-family: 'Amatic SC', cursive; = pour la nav bar-->
-		<link href="https://fonts.googleapis.com/css?family=Abel|Amatic+SC|Josefin+Sans|Quicksand|Yanone+Kaffeesatz" rel="stylesheet">
-	</head>
-<body>
-
-
 	<section>
 
 		<h2>News</h2>
@@ -70,16 +44,5 @@
 				</figcaption>
 			</figure>
 		</div>
-	
-
-
-
-
-
-
-
-
-
-
 
 	</section>
