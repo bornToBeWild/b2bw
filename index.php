26de517541233135e21a5126020d78e117a3c6e2
Valériane Aron <valeriane.aron@gmail.com>
2017-10-18 10:53:45 +0200

    update index.php with includes, update home-news.php wuth figure class, update of style.css with correct img attribute

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:index.html
+
 <?php
 include ("includes/header.php");
 include("includes/home-social.php");
@@ -9,40 +9,3 @@ include("includes/home-news.php");
 include("includes/newsletter.php");
 include("includes/footer.php");
 ?>
-=======
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>BornToBeWild</title>
-
-		<!-- Immport Bootstrap library-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<link rel="stylesheet" href="assets/css/footer.css" />
-
-		<!--Favicon-->
-		<link rel="shortcut icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
-		<link rel="icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
-
-		<!-- Importation des typos Google Fonts, en css : 
-		font-family: 'Quicksand', sans-serif; = pour le corps de texte & Titres TBC
-		font-family: 'Josefin Sans', sans-serif; = divers
-		font-family: 'Yanone Kaffeesatz', sans-serif; = divers
-		font-family: 'Abel', sans-serif; = divers
-		font-family: 'Amatic SC', cursive; = pour la nav bar-->
-		<link href="https://fonts.googleapis.com/css?family=Abel|Amatic+SC|Josefin+Sans|Quicksand|Yanone+Kaffeesatz" rel="stylesheet">
-	</head>
-	<body>
-		<header>
-
-		
-		</header>
-
-			<!--Footer begins here-->
-			<?php include 'includes/footer.php'?>
-
-
-	</body>
-</html>
->>>>>>> 59e8b8a8177f3b1f6ee27f21d20efe44c0040bfe:index.php
