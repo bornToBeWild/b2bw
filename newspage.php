ed891be0ecfff234403aa8fca3b22a96c53eb515
Anysia <abeauzel@orange.fr>
2017-10-19 15:38:51 +0200

    adding the beginning of the newspage

--- newspage.php
+++ newspage.php
@@ -1,63 +1,111 @@
-<?php include ("includes/header.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<!--Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Abel|Amatic+SC|Josefin+Sans|Quicksand|Yanone+Kaffeesatz" rel="stylesheet">
+	
+</head>
 
 
 	<section>
 
-		<h2>Latest News</h2>
+		<div class="newspage_container container">
 
-		<div class="news_container">
+			<!-- ROW 1 -->
 
-			 <figure>
+			<div class="newspage_row row1">
 
-			 	<h3>Titre News 1</h3>
+				<h2>Latest News</h2>
 
-			 	<img class="newspicture" src="assets/pictures/newsPage/News1.jpg" alt="Adrian Von Ziegler">
+					<!-- COL -->
 
-				<figcaption>
-			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
-			 	</figcaption>
-			
-			</figure>
+					<div class="newspage col-md-12">
 
-			 <figure>
+				 		<h3>Titre News 1</h3>
 
-			 	<h3>Titre News 2</h3>
+				 		<div class="picture_news">
 
-			 	<img class="newspicture" src="assets/pictures/newsPage/News2.jpg" alt="Nightwish">
+				 			<img class="newspicture" src="assets/pictures/newsPage/News1.jpg" alt="Adrian Von Ziegler">
 
-				<figcaption>
-			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
-			 	</figcaption>
-			
-			</figure>
+			 				<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>			
+			 			</div>
+					</div>
+			</div>
 
-			 <figure>
+			<!-- END ROW 1 -->
 
-				<h3>Titre News 3</h3>
-			 
-			 	<img class="newspicture" src="assets/pictures/newsPage/News3.jpg" alt="Nightwish">
 
-				<figcaption>
-			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
-			 	</figcaption>
-			
-			</figure>
+			<!-- ROW 2 -->
 
-			 <figure>
-				
-				<h3>Titre News 4</h3>
-			 	
-			 	<img class="newspicture" src="assets/pictures/newsPage/News4.jpg" alt="Rolling Stnes">
+			<div class="newspage_row row2"></div>
 
-				<figcaption>
-			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
-			 	</figcaption>
-			
-			</figure>
+					<!-- COL -->
+
+					<div class="newspage col-md-12">
+
+				 		<h3>Titre News 2</h3>
+
+				 		<div class="picture_news">
+
+				 			<img class="newspicture" src="assets/pictures/newsPage/News2.jpg" alt="Nightwish">
+
+			 				<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>	
+			 			</div>
+					</div>
+			</div>
+
+			<!-- END ROW 2 -->
+					
+			<!-- ROW 3 -->
+
+			<div class="newspage_row row2"></div>
+
+					<!-- COL -->
+
+					<div class="newspage col-md-12">
+
+						<h3>Titre News 3</h3>
+
+					 	<div class="picture_news">
+
+					 		<img class="newspicture" src="assets/pictures/newsPage/News3.jpg" alt="Nightwish">
+
+				 			<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>	
+				 		</div>
+					</div>
+
+			</div>
+
+			<!-- END ROW 3 -->
+
+			<!-- ROW 4 -->
+
+			<div class="newspage_row row2"></div>
+
+					<!-- COL -->
+
+					<div class="newspage col-md-12">
+
+						<h3>Titre News 4</h3>
+
+					 	<div class="picture_news">
+
+					 		<img class="newspicture" src="assets/pictures/newsPage/News4.jpg" alt="Rolling Stnes">
+
+				 			<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>	
+				 		</div>
+					</div>
+			</div>
+
+			<!-- END ROW 2 -->
+
+
 
 		</div>
-
-
-
 
 	</section>
