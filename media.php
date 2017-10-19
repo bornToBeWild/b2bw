4ba1b79bf34511de3580447947734b8c53d5ae68
Valériane Aron <valeriane.aron@gmail.com>
2017-10-19 16:46:37 +0200

    update css and media page

--- media.php
+++ media.php
@@ -12,46 +12,48 @@
 			<h2>Check out our latest videos</h2>
 			<div class= "container-fluid container-video">
 				<div class= "row row-video">
-					<div class="col-md-6">
-						<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/xm5DPlNCmtk?autoplay=0&" frameborder="0" allowfullscreen>
+					<div class="col-md-6 ">
+						<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/xm5DPlNCmtk?autoplay=0&" frameborder="0" allowfullscreen>
 						</iframe>
 					</div>	
-					<div class="col-md-offset-3 col-xs-offset-2">
-						<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/xm5DPlNCmtk?autoplay=0&" frameborder="0" allowfullscreen>
+					<div>
+						<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/fyaI4-5849w?autoplay=0&" frameborder="0" allowfullscreen>
 						</iframe>
 					</div>	
 						
+
 									
 				</div>
 			</div>	
 		</section>
 
 		<section class="mediapage-images">
+			<h2>Sneak peek in pictures of our latest stages...</h2>
 			<div class= "container-fluid container-images">
 				<div class= "row row-images">
 					<div class="col-md-3 col-xs-6">
 						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/bulle.jpg" alt="picture of a festival crowd">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/cindy.jpg" alt="Cindy, le leader de notre groupe">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/doigt.jpg" alt="picture of a festival crowd">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/Electrobeach.jpg" alt="picture of a festival crowd">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/festivalgirl.jpg" alt="picture of a festival crowd">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/Fitz.jpg" alt="picture of a festival crowd">
 					</div>
 					<div class="col-md-3 col-xs-6">
-						<img src="assets/pictures/pictures_media/crowd.jpg" alt="picture of a festival crowd">
+						<img src="assets/pictures/pictures_media/night_festival.jpg" alt="picture of a festival crowd">
 					</div>
 				</div>
 			</div>
