d656a0a15a962f6125f35521952eb7ff1f52e525
Valériane Aron <valeriane.aron@gmail.com>
2017-10-19 10:52:58 +0200

    update of css for home-section-headerbut still not responsive... HELP ! Update of both php files home-section-header.php and home-media.php

--- includes/home-media.php
+++ includes/home-media.php
@@ -1,7 +1,7 @@
 <!-- Section media, iframe youtube video -->
 			<section id="tubecontainer" class="container-fluid">
 				<div class="hometube row">
-					<div class="col-md-offset-2">
+					<div class="col-md-offset-2 col-xs-offset-1">
 						<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/xm5DPlNCmtk?autoplay=0&" frameborder="0" allowfullscreen>
 						</iframe>
 					</div>
