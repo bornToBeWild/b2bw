48540485ad7cb1aba55599d7585cabd84a565596
Anysia <abeauzel@orange.fr>
2017-10-19 14:16:16 +0200

    add new css for footer and homenews, both are now on bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,59 @@
-<footer>
+
+	<footer>
 	
-	<div class="flex_container">
+		<div class="footer_container container">
 
-			<ul class="box1">
-				<li><a href="home">Home</a></li>
-				<li><a href="about">About Us</a></li>
-				<li><a href="news">News</a></li>
-				<li><a href="home">Medias</a></li>
-			</ul>
+			<!-- ROW1 Made with Love -->
+			
+			<div class="footer_row row1">
+				<div class="footer_box col-md-12">
+					<p><a href="#>">Made with <span class="heart">love</span> by B2BW <br /><span class="année">2017<span></a><p>
+				</div>
+			</div>
+
+
+			<!-- ROW2 COL 1 Home / About / News / Medias -->
+
+			<div class="footer_row row2">
+
+				<div class="footer_box col-md-3">
+					<p><a href="home">Home</a></p>
+					<p><a href="about">About Us</a></p>
+					<p><a href="news">News</a></p>
+					<p><a href="medias">Medias</a></p>
+				</div>
+
+			<!-- ROW2 COL 2 Contact / VIDE / FAQ / Legal -->
+
+				<div class="footer_box col-md-3">
+					<p><a href="contact">Contact Us</a></p>
+					<p> </p>
+					<p><a href="#">FAQ</a></p>
+					<p><a href="#">Legal Mentions</a></p>
+				</div>
+
+			<!-- ROW2 COL 3 VIDE / Facebook / Instagram / VIDE -->
+
+				<div class="footer_box col-md-3">
+					<p> </p>
+					<p><a href="facebook">Facebook</a></p>
+					<p><a href="instagram">Instagram</a></p>
+					<p> </p>
+				</div>
+
+
+
+			<!-- ROW2 COL 4 VIDE / Twitter / Spotify / VIDE -->
+
+				<div class="footer_box col-md-3">
+					<p> </p>
+					<p><a href="twitter">Twitter</a></p>			
+					<p><a href="spotify">Spotify</a></p>
+					<p> </p>
+
+				</div>
 		
-			<ul class="box">
-				<li><a href="contact">Contact Us</a></li>
-				<li>FAQ</li>
-				<li>Legal Mentions</li>
-			</ul>
 
-		<div class="with_love">
-			<p class="withloveText">Made with <span class="heart">❤</span> by B2BW<br />2017</p>
 		</div>
 
-			<ul class="box">
-				<li><a href="facebook">Facebook</a></li>
-				<li><a href="instagram">Instagram</a></li>
-			</ul>
-
-			<ul class="box">
-				<li><a href="twitter">Twitter</a></li>
-				<li><a href="spotify">Spotify</a></li>
-			</ul>
-
-	</div>
-
-</footer>
+	</footer>
---
+++ newspage.php
@@ -0,0 +1,63 @@
+<?php include ("includes/header.php");?>
+
+
+	<section>
+
+		<h2>Latest News</h2>
+
+		<div class="news_container">
+
+			 <figure>
+
+			 	<h3>Titre News 1</h3>
+
+			 	<img class="newspicture" src="assets/pictures/newsPage/News1.jpg" alt="Adrian Von Ziegler">
+
+				<figcaption>
+			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
+			 	</figcaption>
+			
+			</figure>
+
+			 <figure>
+
+			 	<h3>Titre News 2</h3>
+
+			 	<img class="newspicture" src="assets/pictures/newsPage/News2.jpg" alt="Nightwish">
+
+				<figcaption>
+			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
+			 	</figcaption>
+			
+			</figure>
+
+			 <figure>
+
+				<h3>Titre News 3</h3>
+			 
+			 	<img class="newspicture" src="assets/pictures/newsPage/News3.jpg" alt="Nightwish">
+
+				<figcaption>
+			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
+			 	</figcaption>
+			
+			</figure>
+
+			 <figure>
+				
+				<h3>Titre News 4</h3>
+			 	
+			 	<img class="newspicture" src="assets/pictures/newsPage/News4.jpg" alt="Rolling Stnes">
+
+				<figcaption>
+			 		<p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum<a href="#">Lire [+]</a></p>
+			 	</figcaption>
+			
+			</figure>
+
+		</div>
+
+
+
+
+	</section>
