67617f8d6f4cc95ad7dab85e4f9038d87fc3236d
Pandora <pandoraangora@gmail.com>
2017-10-19 14:50:52 +0200

    modify agenda clean

--- includes/agenda.php
+++ includes/agenda.php
@@ -1,16 +1,3 @@
-<!-- import juste pour test en local -->
-<!-- Immport Bootstrap library-->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!--CSS-->
-<link rel="stylesheet" href="../assets/css/style.css" />
-<!--Favicon-->
-<link rel="shortcut icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
-<link rel="icon" href="assets/pictures/faviconB.ico" type="image/x-icon">
-
-<!--Fonts-->
-<link href="https://fonts.googleapis.com/css?family=Abel|Amatic+SC|Josefin+Sans|Quicksand|Yanone+Kaffeesatz" rel="stylesheet">
-
 <!-- Agenda -->
 
   <div class="container">
