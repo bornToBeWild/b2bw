c3928349672686c316a906ef8600b8147d0f81ef
Cindy <cindy.liwenge@gmail.com>
2017-10-19 20:47:01 +0200

    Quick fixes

--- legroupe.php
+++ legroupe.php
@@ -9,6 +9,7 @@
   <div class="white_box">
 	<br />
 	<br />
+	<!-- subtitle and cover picture -->
     <section class="theband">
 	    <h2>ABOUT OUR INNER ANIMALS</h2>
 		<img class="img_theband center" src="assets/pictures/members/theband.jpg" alt="Black and white picture of the band onstage, with a large audience">
@@ -17,6 +18,7 @@
   	<br />
   	<br />
 
+  	<!-- story of the band -->
   	<section class="story">
   		<p><span class="everything">Everything happens for a reason.</span> When we met during a casual Halloween party in 2013, we immediatly connected with each others. Four musicians wearing animal costumes and craving for an opportunity to let our inner animals out - how could we ignore such an odd coincidence ? We followed our instinct. The next day we wrote together what would become our first hit, "Who let the leash out (woo woo woo)". And we made a promise to follow our musical dream.</p>
   		<p>Four years, one album, several opening acts, a few trips abroad and a lot of magical encounters later, we're still creating experimental sound with the same passion. Our next common goal is to meet you. We can't wait to start our international tour, because this is why we play. We can't wait to share our new discoveries with you, because this is how we play.</p>
@@ -49,12 +51,12 @@
 
 			<div class="row row_custom">
 				<div class="col-md-offset-1 col-md-4 col-xs-offset-1 col-xs-10 col_member">
-			  			<img src="assets/pictures/members/cat-polaroid.jpg" alt="A Polaroid picture of a yawning cat in "><br />
+			  			<img src="assets/pictures/members/cat-polaroid.jpg" alt="A Polaroid picture of a yawning cat in "><br /><br />
 			  			<p class="animal">Misty 'Duchess' Knight, our singer</p>
 				</div>
 
 				<div class="col-md-offset-2 col-md-4 col-xs-offset-1 col-xs-10 col_member">
-			  			<img src="assets/pictures/members/truc-polaroid.jpeg" alt="An overexposed Polaroid picture of a sleepy sea-lion on the beach by a sunny day"><br />
+			  			<img src="assets/pictures/members/truc-polaroid.jpeg" alt="An overexposed Polaroid picture of a sleepy sea-lion on the beach by a sunny day"><br /><br />
 			  			<p class="animal">Jessica 'Sleepy' Jones, our bassist</p>
 				</div>
 			</div>
@@ -80,4 +82,3 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
