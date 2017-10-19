0246c9906771c909ea8cf42c1cf457d1a93e3bda
Cindy <cindy.liwenge@gmail.com>
2017-10-19 14:31:56 +0200

    modify css start to code on about page and created member pictures file

--- about.php
+++ about.php
@@ -7,14 +7,19 @@
 <body>
 
 	<!--title and picture of the band-->
-	<h2>About our inner animals</h2>
-	<img src="" alt="">
+	<div class="white_box">
 
-	<div class="container ">
-		<div class="row">
-			<div class="about col-md-4 col-md-offset-8 col-xs-11 col-xs-offset-1">
-				
+		<section class="aboutus">
+			<h2>About our inner animals</h2>
+			<img src="" alt="">
+		</section>
+
+		<div class="container ">
+			<div class="row">
+				<div class="about col-md-4 col-md-offset-8 col-xs-11 col-xs-offset-1">
+					
+				</div>
 			</div>
-		</div>
 	</div>
+</section>
 </body>
