d9f6a26a899b516ba911dabd955436654ed2a145
Cindy <cindy.liwenge@gmail.com>
2017-10-19 09:06:29 +0200

    Modified a few lines

--- about.php
+++ about.php
@@ -11,9 +11,10 @@
 	<img src="" alt="">
 
 	<div class="container ">
-		<div class="">
-
-
+		<div class="row">
+			<div class="about col-md-4 col-md-offset-8 col-xs-11 col-xs-offset-1">
+				
+			</div>
 		</div>
 	</div>
 </body>
