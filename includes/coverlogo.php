64697ab06c57dad6d509dbdeb58ec65b50d92a73
Pandora <pandoraangora@gmail.com>
2017-10-19 16:08:00 +0200

    centering coverlogo.php

--- includes/coverlogo.php
+++ includes/coverlogo.php
@@ -1,9 +1,3 @@
-			<section class="cover-section-logo">
-				<div class="container-fluid container-logo">
-					<div class="row row-logo">
-						<div class="col-md-offset-3 col-xs-offset-2">
-							<img src="assets/pictures/logo.png" alt="BornToBeWild logo"/>
-						</div>
-					</div>
-				</div>
-			</section>
+<section class="cover-section-logo">
+	<img src="assets/pictures/logo.png" alt="BornToBeWild logo" class="center"/>
+</section>
