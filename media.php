4891f8bba89d4e1c07f64a0913e7982cac307708
Pandora <pandoraangora@gmail.com>
2017-10-20 14:22:45 +0200

    realign media page

--- media.php
+++ media.php
@@ -8,10 +8,10 @@
 
 	<div class="container white_box">
 		<section class="mediapage">
-			
+
 			<section class="mediapage-video">
 				<h2>Wanna check out our latest videos?</h2>
-		
+
 				<div class= "container-fluid container-video">
 					<div class= "row row-video">
 						<div class="col-md-6 col_video">
@@ -19,11 +19,12 @@
 							<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/xm5DPlNCmtk?autoplay=0&" frameborder="0" allowfullscreen>
 							</iframe>
 						</div>	
-						<div>
+						<div class="col-md-6 col_video">
 							<h3>So much good memories for this gig!</h3>
 							<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/fyaI4-5849w?autoplay=0&" frameborder="0" allowfullscreen>
 							</iframe>
-						</div>	
+						</div>
+					</div>	
 
 					<div class= "row row-video">
 						<div class="col-md-6 col_video">
@@ -31,12 +32,12 @@
 							<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/xm5DPlNCmtk?autoplay=0&" frameborder="0" allowfullscreen>
 							</iframe>
 						</div>	
-						<div>
+						<div class="col-md-6 col_video">
 							<h3>First festival for the Born to be wild</h3>
 							<iframe id="ytplayer" type="text/html" width="560" height="315" src="https://www.youtube.com/embed/fyaI4-5849w?autoplay=0&" frameborder="0" allowfullscreen>
 							</iframe>
 						</div>			
-					
+
 					</div>
 				</div>	
 			</section>
@@ -70,7 +71,7 @@
 							<img src="assets/pictures/pictures_media/festivalgirl.jpg" alt="picture of a festival crowd">
 						</div>
 					</div>
-					
+
 					<div class= "row row-images">
 						<div class="col-md-6 col-xs-6">
 							<img src="assets/pictures/pictures_media/Fitz.jpg" alt="picture of a festival crowd">
@@ -78,14 +79,14 @@
 						<div class="col-md-6 col-xs-6">
 							<img src="assets/pictures/pictures_media/night_festival.jpg" alt="picture of a festival crowd">
 						</div>
-						
+
 					</div>
 				</div>
 			</section>
 		</section>
 	</div>
 	<!-- footer -->
-<?php include("includes/footer.php");?>
+	<?php include("includes/footer.php");?>
 
 </body>
 
